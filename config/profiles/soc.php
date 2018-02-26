5d60e2b44846ff187ad684acbc07ae0a0bc88c31
Mike Kamornikov <mikekamornikov@gmail.com>
2018-02-26 14:42:47 +0300

    added SOC profile

---
+++ config/profiles/soc.php
@@ -0,0 +1,48 @@
+<?php
+
+$modules = array(
+    'Tags' => 500,
+    'EmailAddresses' => 100,
+    'ACLRoles' => 10,
+    'Users' => 10000,
+    'Teams' => 10000,
+    'Accounts' => 65000,
+    'Quotes' => 100,
+    'ProductBundles' => 10000,
+    'Products' => 10000,
+    'Calls' => 100,
+    'Emails' => 100,
+    'EmailText' => 10,
+    'Contacts' => 85000,
+    'Leads' => 100,
+    'Opportunities' => 75000,
+    'Cases' => 100,
+    'Bugs' => 0,
+    'Meetings' => 100,
+    'Tasks' => 100,
+    'Notes' => 100,
+    'Documents' => 10,
+    'Categories' => 100,
+    'KBContents' => 0,
+    'SugarFavorites' => 5000,
+    'Reports' => 100,
+    'RevenueLineItems' => 75000,
+);
+
+$sugarFavoritesModules = array(
+    'Accounts'      => 6500, // 10% of base accounts
+    'Contacts'      => 8500,
+    'Leads'         => 10,
+    'Opportunities' => 7500,
+    'RevenueLineItems' => 7500,
+    'Calls'         => 5, // 5% of base calls
+    'Meetings'      => 5, // 5% of base meetings
+    'Cases'         => 5,
+    'Bugs'          => 0,
+    'Tasks'         => 5,
+    'Notes'         => 5,
+);
+
+$profile_opts = array(
+    
+);
