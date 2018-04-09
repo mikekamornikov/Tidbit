907f3147eeab13a16e50b867ca27a45552714788
Mike Kamornikov <mikekamornikov@gmail.com>
2018-04-09 18:51:50 +0300

    added tiny SOC profile

---
+++ config/profiles/tiny_soc.php
@@ -0,0 +1,48 @@
+<?php
+
+$modules = array(
+    'Tags' => 100,
+    'EmailAddresses' => 100,
+    'ACLRoles' => 10,
+    'Users' => 10,
+    'Teams' => 10,
+    'Accounts' => 100,
+    'Quotes' => 100,
+    'ProductBundles' => 100,
+    'Products' => 100,
+    'Calls' => 100,
+    'Emails' => 100,
+    'EmailText' => 10,
+    'Contacts' => 100,
+    'Leads' => 100,
+    'Opportunities' => 100,
+    'Cases' => 100,
+    'Bugs' => 0,
+    'Meetings' => 100,
+    'Tasks' => 100,
+    'Notes' => 100,
+    'Documents' => 10,
+    'Categories' => 100,
+    'KBContents' => 0,
+    'SugarFavorites' => 100,
+    'Reports' => 100,
+    'RevenueLineItems' => 100,
+);
+
+$sugarFavoritesModules = array(
+    'Accounts'      => 10, // 10% of base accounts
+    'Contacts'      => 10,
+    'Leads'         => 10,
+    'Opportunities' => 10,
+    'RevenueLineItems' => 10,
+    'Calls'         => 5, // 5% of base calls
+    'Meetings'      => 5, // 5% of base meetings
+    'Cases'         => 5,
+    'Bugs'          => 0,
+    'Tasks'         => 5,
+    'Notes'         => 5,
+);
+
+$profile_opts = array(
+    
+);
